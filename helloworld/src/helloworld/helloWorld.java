6c0202b48fe89a70ca476dc8540bc383875a0e59
ishwarya <ishwaryab@newtglobal.com>
2016-04-04 11:59:21 +0530

    jenkins sample

---
+++ helloworld/src/helloworld/helloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package helloworld;
+
+/**
+ * @author ishwarya
+ *
+ */
+public class helloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello Ishu");
+
+	}
+
+}
